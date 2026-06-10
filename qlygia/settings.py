57c660916d392663146cbe6a021719cfefc033df
Tuinagaf <anhpq.dev@gmail.com>
2026-06-10 09:25:12 +0700

    Revert to SQLite only

--- qlygia/settings.py
+++ qlygia/settings.py
@@ -75,21 +75,13 @@ WSGI_APPLICATION = 'qlygia.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-import dj_database_url
-
-# Default to SQLite for development
+# Default to SQLite
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-# Override with PostgreSQL if DATABASE_URL is configured in .env or server environment
-if config('DATABASE_URL', default=''):
-    DATABASES['default'] = dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
 
 
 # Password validation
